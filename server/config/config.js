c9ce03cd8dad754b9e1fff22996b05703705cd6e
shobhitsrivastava <shobhit@gatech.edu>
2017-03-02 12:29:39 -0500

    added start script for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -8,5 +8,7 @@
 //     });
 // }
 
-process.env.NODE_ENV = 'development';
-process.env.MONGODB_URI = 'mongodb://localhost:27017/LendingLegend';
+if (process.env.NODE_ENV !== 'production') {
+    process.env.NODE_ENV = 'development';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/LendingLegend';
+}
